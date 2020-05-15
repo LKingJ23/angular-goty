4703807bed37f30d01f460f89ea387da4bd85d32
Miguel Hidalgo Rey <lkingj23@gmail.com>
2020-05-15 08:25:45 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,11 @@ export const environment = {
   production: true,
   url: 'https://us-central1-firestore-grafica-62eab.cloudfunctions.net',
   firebase: {
-    apiKey: 'AIzaSyCLGOT0MU5cgepbSXhB3kx9G5TIO92qnCM',
-    authDomain: 'firestore-grafica-62eab.firebaseapp.com',
-    databaseURL: 'https://firestore-grafica-62eab.firebaseio.com',
-    projectId: 'firestore-grafica-62eab',
-    storageBucket: 'firestore-grafica-62eab.appspot.com',
-    messagingSenderId: '365290155336',
-    appId: '1:365290155336:web:93a69666a7396e9507d77d'
+    apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
   }
 };
