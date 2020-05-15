04a0cba31e0384bbefabb27ed469fad14bf17fd1
LKingJ23 <lkingj23@gmail.com>
2020-05-15 08:10:29 +0200

    front app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://us-central1-firestore-grafica-62eab.cloudfunctions.net',
+  firebase: {
+    apiKey: 'AIzaSyCLGOT0MU5cgepbSXhB3kx9G5TIO92qnCM',
+    authDomain: 'firestore-grafica-62eab.firebaseapp.com',
+    databaseURL: 'https://firestore-grafica-62eab.firebaseio.com',
+    projectId: 'firestore-grafica-62eab',
+    storageBucket: 'firestore-grafica-62eab.appspot.com',
+    messagingSenderId: '365290155336',
+    appId: '1:365290155336:web:93a69666a7396e9507d77d'
+  }
 };
